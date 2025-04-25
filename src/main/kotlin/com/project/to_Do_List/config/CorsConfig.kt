b334766b7e256a96acfede33f584d4195d807359
Gabriel <gabrielconceicao569@gmail.com>
2025-04-25 15:58:18 -0300

    Update

--- src/main/kotlin/com/project/to_Do_List/config/CorsConfig.kt
+++ src/main/kotlin/com/project/to_Do_List/config/CorsConfig.kt
@@ -13,7 +13,7 @@ class CorsConfig {
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**")
                     .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE") 
             }
         }
     }
